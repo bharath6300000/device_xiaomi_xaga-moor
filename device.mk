62ea24ba09b95b569df4397a0e7b0ada906beee3
bharath6300000 <131575410+bharath6300000@users.noreply.github.com>
2023-06-23 15:09:32 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -40,10 +40,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
     
 # MIUI Camera
-PRODUCT_SYSTEM_PROPERTIES += \
-    ro.miui.notch=1 \
-    ro.product.mod_device=xaga_global \
-    camera.lab.options=true
+# PRODUCT_SYSTEM_PROPERTIES += \
+   # ro.miui.notch=1 \
+   # ro.product.mod_device=xaga_global \
+   # camera.lab.options=true
 
 # Overlays
 PRODUCT_PACKAGES += \
