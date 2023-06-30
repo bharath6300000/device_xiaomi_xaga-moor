1d68d3b799b5a81a816588d003adeadefeaa5874
Daemon Targaryen <131575410+bharath6300000@users.noreply.github.com>
2023-06-30 10:33:18 -0700

    Update and rename cherish_xaga.mk to lineage_xaga.mk

--- lineage_xaga.mk
+++ lineage_xaga.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common cherish configurations
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from xaga device
 $(call inherit-product, device/xiaomi/xaga/device.mk)
 
 PRODUCT_DEVICE := xaga
-PRODUCT_NAME := cherish_xaga
+PRODUCT_NAME := lineage_xaga
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note11T Pro
 PRODUCT_MANUFACTURER := Xiaomi
